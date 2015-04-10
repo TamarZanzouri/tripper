ecb5927d0ef33088f26ebb0ecba058335f26d39e
haimyyy <haimyyy@gmail.com>
2015-04-10 11:46:56 +0300

    haim

--- index.js
+++ index.js
@@ -1,129 +1,127 @@
-
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
-var app=express();
-var Character = ['char1','char2','char3','char4','char1','char2','char3','char4','char1','char2','char3','char4'];
+var app = express();
+var Character = ['char1', 'char2', 'char3', 'char4', 'char1', 'char2', 'char3', 'char4', 'char1', 'char2', 'char3', 'char4'];
 
-app.set('view engine' ,'ejs');
-app.set('views',path.join(__dirname , 'views'));
-app.use(express.static(path.join(__dirname,'node_modules/bower_components')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'node_modules/bower_components')));
 
-app.use(express.static(path.join(__dirname,'tripFunction')));
-app.use(express.static(path.join(__dirname,'style')));
+app.use(express.static(path.join(__dirname, 'tripFunction')));
+app.use(express.static(path.join(__dirname, 'style')));
 
 app.use(bodyParser());
 
-
-
 //view in data base
-app.get('/',function(req,res){
+app.get('/', function(req, res) {
 	// Connect to the db
 	MongoClient.connect("mongodb://TripperDB:shenkar6196@ds041177.mongolab.com:41177/tripperbd", function(err, db) {
-	  if(err) { 
-	  	return console.dir(err);
-	 }
-	 else{
-		var tripper_collection = db.collection('tripper_playlist');
-		tripper_collection.find({ }).toArray(function (err, docs) {
-			console.log(docs);
-			console.log(docs.trip_charachters);
-		    res.render('index.ejs',{
-				title:'Tripper',
-				character:Character,
-				trips:docs
-				// charachters : docs.trip_charachters
+		if (err) {
+			return console.dir(err);
+		} else {
+			var tripper_collection = db.collection('tripper_playlist');
+			tripper_collection.find({ }).toArray(function(err, docs) {
+				console.log(docs);
+				console.log(docs.trip_charachters);
+				res.render('index.ejs', {
+					title : 'Tripper',
+					character : Character,
+					trips : docs
+					// charachters : docs.trip_charachters
+				});
 			});
-		 });
 		}
 	});
-	
+
 });
 
-app.post('/add', function (req,res) {
-	console.log("haim"+req.body.newTrip+" "+req.body.des);
+app.post('/add', function(req, res) {
+	console.log("haim" + req.body.newTrip + " " + req.body.des);
 	MongoClient.connect("mongodb://TripperDB:shenkar6196@ds041177.mongolab.com:41177/tripperbd", function(err, db) {
-	  if(err) { return console.dir(err);}
-	 else{
-		var tripper_collection = db.collection('tripper_playlist');
-		var nameTrip = req.body.nameTrip;
-		var des = req.body.des;
-		var location = req.body.locationTrip;
-		var char1 = req.body.char1;
-		var char2 = req.body.char2;
-		var privte =req.body.privte;
-		// var temp = {
-		// 	id: new Date().getTime(),
-		// 	name:nameTrip,
-		// 	description:des,
-		// 	location:location,
-		// 	character1:char1,
-		// 	character2:char2,
-		// 	ifPrivte:privte
-		// };
-		//console.log(temp);
-		tripper_collection.insert({
-			trip_name : nameTrip,
-			trip_description : des,
-			address : location,
-			trip_charachters : [{
-				charachter : char1
-			},
-			{ 
-				charachter : char2
-			}],
-			trip_isPrivate : privte
-		}, function(err, docs){
-			if(err){
-				console.log("found error inserting");
-				db.close();
-				return console.error(err);
-			}
-			console.log("inserted:");
-			for(var i in docs){
-				console.log(docs[i]);
-			}
-			
-			console.log("inserted " + docs.length + documents);
-
-			tripper_collection.findOne({ _id : new ObjectId()}, function(err, doc){
-				if(err)
+		if (err) {
+			return console.dir(err);
+		} else {
+			var tripper_collection = db.collection('tripper_playlist');
+			var nameTrip = req.body.nameTrip;
+			var des = req.body.des;
+			var location = req.body.locationTrip;
+			var char1 = req.body.char1;
+			var char2 = req.body.char2;
+			var privte = req.body.privte;
+			// var temp = {
+			// 	id: new Date().getTime(),
+			// 	name:nameTrip,
+			// 	description:des,
+			// 	location:location,
+			// 	character1:char1,
+			// 	character2:char2,
+			// 	ifPrivte:privte
+			// };
+			//console.log(temp);
+			tripper_collection.insert({
+				trip_name : nameTrip,
+				trip_description : des,
+				address : location,
+				trip_charachters : [{
+					charachter : char1
+				}, {
+					charachter : char2
+				}],
+				trip_isPrivate : privte
+			}, function(err, docs) {
+				if (err) {
+					console.log("found error inserting");
+					db.close();
 					return console.error(err);
-				console.log("read 1 item" + doc);
+				}
+				console.log("inserted:");
+				for (i in docs) {
+					console.log(docs[i]);
+				}
+
+				console.log("inserted " + docs.length + documents);
+
+				tripper_collection.findOne({
+					_id : new ObjectId()
+				}, function(err, doc) {
+					if (err)
+						return console.error(err);
+					console.log("read 1 item" + doc);
+				});
 			});
-		});
-	}
-	  // var doc1 = {'hello':'doc1'};
-	  // var doc2 = {'hello':'doc2'};
-	  // var lotsOfDocs = [{'hello':'doc3'}, {'hello':'doc4'}];
+		}
+		// var doc1 = {'hello':'doc1'};
+		// var doc2 = {'hello':'doc2'};
+		// var lotsOfDocs = [{'hello':'doc3'}, {'hello':'doc4'}];
 
-	  // collection.insert(doc1);
+		// collection.insert(doc1);
 
-	  // collection.insert(doc2, {w:1}, function(err, result) {});
+		// collection.insert(doc2, {w:1}, function(err, result) {});
 
-	  // collection.insert(lotsOfDocs, {w:1}, function(err, result) {});
+		// collection.insert(lotsOfDocs, {w:1}, function(err, result) {});
 	});
 	res.redirect('/');
-	/*	
-    res.render('add.ejs',{
-		titel:'Tripper',
-		trips:docs
-		});
-	});*/
+	/*
+	 res.render('add.ejs',{
+	 titel:'Tripper',
+	 trips:docs
+	 });
+	 });*/
 });
 
 //view in data base
-app.get('/click',function(req,res){
+app.get('/click', function(req, res) {
 
 	res.render('click.ejs')
 });
 
-app.get('/*',function(req,res){
+app.get('/*', function(req, res) {
 
-	res.send(404,"aaaaaaaa")
+	res.send(404, "aaaaaaaa")
 });
 port = app.get('port') || 1337
-app.listen(port,function(){
-	console.log("port "+port);
+app.listen(port, function() {
+	console.log("port " + port);
 });
