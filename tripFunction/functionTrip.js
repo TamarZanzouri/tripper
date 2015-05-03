91554e91002e3a42d3ec35954569772b5742dac9
haimyyy <haimyyy@gmail.com>
2015-05-04 01:49:03 +0300

    changed to heruko

--- tripFunction/functionTrip.js
+++ tripFunction/functionTrip.js
@@ -1,5 +1,6 @@
 User={};
-g_domain="http://127.0.0.1:1337/";//"http://shenkartripper.herokuapp.com/";//
+g_domain="http://shenkartripper.herokuapp.com/";
+// "http://127.0.0.1:1337/";
 g_trip={};
 g_ListTrip=[];
 var filter = [];
