3f4d11d6c44645dfb611c7da1e6c1ad1d34d69d2
Tamar Zanzouri <zanzouritamar@gmail.com>
2015-06-03 23:42:52 +0300

    trying to fix my file

--- tripFunction/functionTrip.js
+++ tripFunction/functionTrip.js
@@ -1790,7 +1790,7 @@ console.log(form)
 		moveToAccountPage();
 	}
 	else{
-		form.append("flag", "favorites");
+		form.append("flag", "favorites")
 		$.ajax({
 			type: "post",
 		        url: g_domain+"add",// where you wanna post
@@ -2007,70 +2007,60 @@ function signinCallback(authResult) {
         } 
     });
 	}
-function displayListTrip(data){
-	console.log(data)
-	// if (e.originalEvent.newURL.indexOf("#myPageSchedule") != -1) {
-	// 	console.log("home")
-	// }
-	$('.userDetailes').empty();
-	if (User) {
-		// var divUser = $('<div>').addClass('userDetailes');
-		var spanUser = $('<span>').html(User.name).addClass('nameUser');
-		var imgUser = $('<img>').attr("src",User.image).addClass('imgUser');
-		$('.userDetailes').append(imgUser)
-		$('.userDetailes').append(spanUser)
-	}
-	/**** data to result page *****/
-		$('#who_are_you_going_with div button').html(TRIPPER_DATA.who_are_you_going_with);
-		$('label[for="trip_with_animals"]').text(TRIPPER_DATA.trip_with_animals);
-		$('label[for="trip_with_road"]').text(TRIPPER_DATA.trip_with_road);
-		$('label[for="trip_for_children"]').text(TRIPPER_DATA.trip_for_children);
-		
-		$('#trip_kind div button').html(TRIPPER_DATA.trip_kind);
-		$('label[for="trip_with_watter"]').text(TRIPPER_DATA.trip_with_watter);
-		$('label[for="trip_with_bicycle"]').text(TRIPPER_DATA.trip_with_bicycle);
-		$('label[for="trip_with_jeep"]').text(TRIPPER_DATA.trip_with_jeep);
-		$('label[for="trip_for_day"]').text(TRIPPER_DATA.trip_for_day);
-		$('label[for="trip_for_night"]').text(TRIPPER_DATA.trip_for_night);
-		
-		// $('#dificullty button span').html(TRIPPER_DATA.dificullty);
-		// $('#light_trip a').html(TRIPPER_DATA.light_trip);
-		// $('#medium_trip a').html(TRIPPER_DATA.medium_trip);
-		// $('#hard_trip a').html(TRIPPER_DATA.hard_trip);
-		
-		// $('#area button span').html(TRIPPER_DATA.area);
-		
-
-	/****end data to result page *****/
-	if(window.location.hash=='#viewFavorite')
-	{
-		favoriteFlagList=1;
-		$('#resultTitleFavorite').html(TRIPPER_DATA.resultTitleFavorite)
-	}
-	if(window.location.hash=='#accountPage')
-	{
-		accountCounter=1;
-	}
-	console.log(accountCounter)
-	if (accountCounter==0){
-		$('#resultTitle').html(TRIPPER_DATA.resultTitle);
-		if(janers.length==1){
-			$('#chars').html(janers[0]);
-		}else{
-			$('#chars').html(janers[0]+" + "+janers[1])
+	function displayListTrip(data){
+		console.log(data)
+		// if (e.originalEvent.newURL.indexOf("#myPageSchedule") != -1) {
+		// 	console.log("home")
+		// }
+		$('.userDetailes').empty();
+		if (User) {
+			// var divUser = $('<div>').addClass('userDetailes');
+			var spanUser = $('<span>').html(User.name).addClass('nameUser');
+			var imgUser = $('<img>').attr("src",User.image).addClass('imgUser');
+			$('.userDetailes').append(imgUser)
+			$('.userDetailes').append(spanUser)
 		}
-	};
-	$('.displayTripRight').empty();
-	$('.displayTripLeft').empty();
-	$('#filtermenu').show();
-	var ulR = $('.displayTripRight');
-	var ulL = $('.displayTripLeft')
-	trip=data;
-	if (data.length==0){
-		console.log("haimaajn");
-		$('#resultTitle').html(TRIPPER_DATA.noResult);
-		$('#filtermenu').hide();
-	}else{
+		/**** data to result page *****/
+			$('#who_are_you_going_with div button').html(TRIPPER_DATA.who_are_you_going_with);
+			$('label[for="trip_with_animals"]').text(TRIPPER_DATA.trip_with_animals);
+			$('label[for="trip_with_road"]').text(TRIPPER_DATA.trip_with_road);
+			$('label[for="trip_for_children"]').text(TRIPPER_DATA.trip_for_children);
+			
+			$('#trip_kind div button').html(TRIPPER_DATA.trip_kind);
+			$('label[for="trip_with_watter"]').text(TRIPPER_DATA.trip_with_watter);
+			$('label[for="trip_with_bicycle"]').text(TRIPPER_DATA.trip_with_bicycle);
+			$('label[for="trip_with_jeep"]').text(TRIPPER_DATA.trip_with_jeep);
+			$('label[for="trip_for_day"]').text(TRIPPER_DATA.trip_for_day);
+			$('label[for="trip_for_night"]').text(TRIPPER_DATA.trip_for_night);
+			
+			// $('#dificullty button span').html(TRIPPER_DATA.dificullty);
+			// $('#light_trip a').html(TRIPPER_DATA.light_trip);
+			// $('#medium_trip a').html(TRIPPER_DATA.medium_trip);
+			// $('#hard_trip a').html(TRIPPER_DATA.hard_trip);
+			
+			// $('#area button span').html(TRIPPER_DATA.area);
+			
+
+		/****end data to result page *****/
+		if(window.location.hash=='#viewFavorite')
+		{
+			favoriteFlagList=1;
+			$('#resultTitleFavorite').html(TRIPPER_DATA.resultTitleFavorite)
+		}
+		if(window.location.hash=='#accountPage')
+		{
+			accountCounter=1;
+		}
+		console.log(accountCounter)
+		if (accountCounter==0){
+			$('#resultTitle').html(TRIPPER_DATA.resultTitle);
+			$('#chars').html(janers[0]+" + "+janers[1]);
+		};
+		$('.displayTripRight').empty();
+		$('.displayTripLeft').empty();
+		var ulR = $('.displayTripRight');
+		var ulL = $('.displayTripLeft')
+		trip=data;
 		$.each(data,function(index,val){
 			var li = $('<li>');
 			li.attr({"id":val._id}).addClass('listResultTrip trip');
@@ -2135,7 +2125,6 @@ function displayListTrip(data){
 				ulL.append(li);
 			}
 		});
-	}
 favoriteFlagList=0;
 accountCounter=0;
 console.log("after display list",accountCounter)
@@ -2347,13 +2336,14 @@ $(document).on('click','#editFavorite',function(){
 			};
 			if ((i+1)>1) {
 				console.log(i);
+				console.log(val.location,val.siteName);
 				$('#newChild'+(i+1)).val(val.siteName);
-				$('#thumbnil'+(i+1)).attr("src",val.img);
+				$('.amount'+(i+1)).val(val.location);
 			};
 		});
 	}
-	// $('#imgUpload').focus();
-	// $('#thumbnil').attr("src",g_trip.imageUrl);
+	$('#imgUpload').focus();
+	$('#thumbnil').attr("src",g_trip.imageUrl);
 	$('#dvMap').click(function (){
 
 	});
@@ -2416,22 +2406,20 @@ $(document).on('click','#editTripFromAccount',function(){
 	var len = g_trip.tripSites.length;
 	
 	if(len){
-		debugger;
 		$.each(g_trip.tripSites,function(i,val){
 			if ((i+1)==1 ) {
 				firstSites()
 			};
 			if ((i+1)>1) {
-				debugger;
 				console.log(i);
-				console.log("haim",val.img,val.siteName);
+				console.log(val.location,val.siteName);
 				$('#newChild'+(i+1)).val(val.siteName);
-				$('#thumbnil'+(i+1)).attr("src",val.img);
+				$('.amount'+(i+1)).val(val.location);
 			};
 		});
 	}
-	// $('#imgUpload').focus();
-	// $('#thumbnil').attr("src",g_trip.imageUrl);
+	$('#imgUpload').focus();
+	$('#thumbnil').attr("src",g_trip.imageUrl);
 	$('#dvMap').click(function (){
 
 	});
@@ -2447,8 +2435,7 @@ $(document).on('click','#editTripFromAccount',function(){
 });
 function firstSites(){
 	$('.firstIngredient').trigger('focus').val(g_trip.tripSites[0].siteName);
-	console.log(g_trip.tripSites[0].img)
-	$('#thumbnil').attr("src",g_trip.tripSites[0].img);
+	$('.amount1').val(g_trip.tripSites[0].location);
 }
 
 $(document).on('click','.btn-primary',function(){
