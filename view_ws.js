e96d1fe0b2fee6097e59851afcb959eed68c5b75
Tamar Zanzouri <zanzouritamar@gmail.com>
2015-06-09 23:06:03 +0300

    bring all the data together, changing overflow hidden in timeline pictures

--- view_ws.js
+++ view_ws.js
@@ -1,25 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/getTripCharachters', function (req, res) {
-
-	console.log("in first function")
-
-	db.model('tripper_views').find({ _id : new ObjectId('554cd2d4e4b0e50ebd74101b')}, function (err, docs) {
-		console.log(docs)
-		res.json(docs)
-	});
-
-	})
 router.get('/getTripData', function (req, res) {
 
 	console.log("in first function")
 
-	db.model('tripper_views').find({ _id : new ObjectId('556b7d36e4b03d041b59acfe')}, function (err, docs) {
-		console.log(docs[0])
-		res.json(docs[0])
+	db.model('tripper_views').find({}, function (err, docs) {
+		console.log("0", docs[0], "1\n", docs[1])
+		res.json({tripCharachters : docs[0], tripData : docs[1]})
 	});
 
-	})
+})
 
 module.exports = router;
