0bff8b7d0d4c55a3f068518c0c5ad92ce8d2802a
Tamar Zanzouri <zanzouritamar@gmail.com>
2015-06-13 12:05:58 +0300

    changed design of user login

--- view_ws.js
+++ view_ws.js
@@ -1,18 +1,4 @@
-var express = require('express');
-var router = express.Router();
-
-
-
-// router.get('/getTripData', function (req, res) {
-
-// 	console.log("in first function")
-
-// 	db.model('tripper_views').find({}, function (err, docs) {
-// 		console.log("0", docs[0], "1\n", docs[1])
-// 		res.json({tripCharachters : docs[0], tripData : docs[1]})
-// 	});
-
-// })
+// var express = require('express');
 
 exports.getTripData =  function (req, res) {
 
@@ -24,6 +10,3 @@ exports.getTripData =  function (req, res) {
 	});
 
 }
-
-
-// module.exports = router;
